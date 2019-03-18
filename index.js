23920a7ffef58b6ec0f6dd96c84b557099285237
Rony Fragin <rony.fragin@guardicore.com>
2019-03-18 11:43:50 +0200

    add more granular options

--- index.js
+++ index.js
@@ -28,28 +28,31 @@ module.exports = function(source, sourcemap) {
 
   Object.assign(config, query);
 
-  if (config.keepUrl === true) {
-      styleProperty = 'styleUrls';
-      templateProperty = 'templateUrl';
+  if (config.keepUrl === true && !config.keepStyleUrl) {
+    styleProperty = 'styleUrls';
   }
 
-    // Not cacheable during unit tests;
+  if (config.keepUrl === true && !config.keepTemplateUrl) {
+    templateProperty = 'templateUrl';
+  }
+
+  // Not cacheable during unit tests;
   this.cacheable && this.cacheable();
 
   var newSource = source.replace(templateUrlRegex, function (match, url) {
-                 // replace: templateUrl: './path/to/template.html'
-                 // with: template: require('./path/to/template.html')
-                 // or: templateUrl: require('./path/to/template.html')
-                 // if `keepUrl` query parameter is set to true.
-                 return templateProperty + ":" + replaceStringsWithRequires(url);
-               })
-               .replace(stylesRegex, function (match, urls) {
-                 // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
-                 // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
-                 // or: styleUrls: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
-                 // if `keepUrl` query parameter is set to true.
-                 return styleProperty + ":" + replaceStringsWithRequires(urls);
-               });
+    // replace: templateUrl: './path/to/template.html'
+    // with: template: require('./path/to/template.html')
+    // or: templateUrl: require('./path/to/template.html')
+    // if `keepUrl` or `keepTemplateUrl` query parameter is set to true.
+    return templateProperty + ":" + replaceStringsWithRequires(url);
+  })
+    .replace(stylesRegex, function (match, urls) {
+      // replace: stylesUrl: ['./foo.css', "./baz.css", "./index.component.css"]
+      // with: styles: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
+      // or: styleUrls: [require('./foo.css'), require("./baz.css"), require("./index.component.css")]
+      // if `keepUrl` or `keepStyleUrl` query parameter is set to true.
+      return styleProperty + ":" + replaceStringsWithRequires(urls);
+    });
 
   // Support for tests
   if (this.callback) {
